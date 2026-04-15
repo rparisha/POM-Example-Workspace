4342d5ee1a03dafdc6bf38fa706ae4f9d6529f54
Hemanth Vanchanagiri <ranjithparsha@gmail.com>
2026-04-15 17:44:28 +0530

    New negative test added

--- tests/loginSauce.spec.ts
+++ tests/loginSauce.spec.ts
@@ -3,7 +3,6 @@ import { LoginSauceDemoPage } from '../pages/LoginSauceDemoPage';
 import { testData } from '../utils/testData';
 
 test('Login with valid credentials', async ({ page }) => {
-
     const loginPage = new LoginSauceDemoPage(page);
     await loginPage.goto();
     console.log('Navigated to Sauce Demo login page');
@@ -11,4 +10,13 @@ test('Login with valid credentials', async ({ page }) => {
     console.log('Entered valid credentials and clicked login');    
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     console.log('Login successful, navigated to inventory page');
+});
+test ('Login with invalid credentials', async ({ page }) => {
+    const loginPage = new LoginSauceDemoPage(page);
+    await loginPage.goto();
+    console.log('Navigated to Sauce Demo Loginpage');
+    await loginPage.login(testData.invalidSauceUser.username, testData.invalidSauceUser.password);
+    console.log('Entered invalid credentials and clicked login');
+    await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Username and password do not match any user in this service');
+    console.log('Verified error message is displayed for invalid login');
 });
